9e7826386bc801fe3a23fac948ec086743f185fb
integerJI <jjs9536@gmail.com>
2020-06-21 14:20:19 +0900

    동적파일설정

--- timeProject/urls.py
+++ timeProject/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import timeUser.views
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     path('', timeUser.views.signin, name='signin'),
     path('timeApp/', include('timeApp.urls')),
     path('timeUser/', include('timeUser.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
