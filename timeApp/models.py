394ca905803e383de796ad67d4196c3b61c9f252
integerJI <jjs9536@gmail.com>
2020-06-23 21:08:20 +0900

    모델 설정

--- timeApp/models.py
+++ timeApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Timesave(models.Model):
+    save_user = models.ForeignKey(User, on_delete = models.CASCADE)
+    save_date = models.IntegerField()
+
+    def __str__(self):
+        return '%s - %s' % (self.save_user, self.save_date) 
--- timeApp/views.py
+++ timeApp/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib.auth.models import User
+from .models import Timesave
+try:
+    from django.utils import simplejson as json
+except ImportError:
+    import json
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 
 def index(request):
     return render(request, 'index.html')
 
 def home(request):
     return render(request, 'home.html')
+
+@login_required
+@require_POST
+def timesave(request):
+    if request.method == 'POST':
+        
+        timesave = Timesave()
+        timesave.save_user = User.objects.get(username = request.user.get_username())
+        timesave.save_date = request.POST.get('time')
+        timesave.save()
+
+        return HttpResponse(content_type='application/json')
