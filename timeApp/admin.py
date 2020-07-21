ff556fc0f7d8ac84d8a09bbe856df4afd54b504b
integerJI <jjs9536@gmail.com>
2020-07-21 23:51:09 +0900

    공지사항 조회수 추가

--- timeApp/admin.py
+++ timeApp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Timesave
+from .models import Timesave, Notices
 
 admin.site.register(Timesave)
+admin.site.register(Notices)
--- timeApp/models.py
+++ timeApp/models.py
@@ -11,3 +11,17 @@ class Timesave(models.Model):
     def __str__(self):
         return '%s - %s' % (self.save_user, self.save_date) 
 
+class Notices(models.Model):
+    objects = models.Manager()
+    n_title = models.CharField(max_length=100)
+    n_body = models.TextField()
+    n_hit = models.PositiveIntegerField(default=0)
+    n_input_date = models.DateTimeField('date published', default=timezone.now)
+
+    def __str__(self):
+        return self.n_title
+
+    @property
+    def update_counter(self):
+        self.n_hit = self.n_hit + 1
+        self.save()
