6741037e33210b7811f96ca443a472b6e7830c92
integerJI <jjs9536@gmail.com>
2020-07-23 22:39:45 +0900

    기본페이지 index로 수정

--- timeProject/urls.py
+++ timeProject/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import timeUser.views
+import timeApp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', timeUser.views.signin, name='signin'),
+    path('', timeApp.views.index, name='index'),
     path('timeApp/', include('timeApp.urls')),
     path('timeUser/', include('timeUser.urls')),
 ]
